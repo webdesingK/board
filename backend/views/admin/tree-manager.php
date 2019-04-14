835c7d7727df7997dd5fe9420aa22bd5cd28e277
игорь кудрявцев <igorayn_112@mail.ru>
2019-04-14 11:13:16 +0300

    aasda

--- backend/views/admin/tree-manager.php
+++ backend/views/admin/tree-manager.php
@@ -4,14 +4,11 @@ $this->title = 'Манагер меню';
 
 
 <div class="category">
-<<<<<<< HEAD
 	<div class="category__list">
 	sdsd
 	</div>
 	<!-- /.category__list -->
-=======
 
     vasa
->>>>>>> 25ec1636758a38c9ab89ae7e79697275f5f28317
 </div>
 <!-- /.category -->
