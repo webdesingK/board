09f9102700b77d1016dc1087fb2df074c90dd023
игорь кудрявцев <igorayn_112@mail.ru>
2019-04-14 11:41:43 +0300

    asd

--- backend/views/admin/tree-manager.php
+++ backend/views/admin/tree-manager.php
@@ -3,11 +3,7 @@ $this->title = 'Манагер меню';
 ?>
 
 
-<<<<<<< HEAD
 <div class="category dog">
-=======
-<div class="category categ">
->>>>>>> backend
 	<div class="category__list">
  Lorem ipsum dolor sit amet.
 	</div>
