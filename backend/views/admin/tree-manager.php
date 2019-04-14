91227f8d7d72371826a9a1bd10a5daa1566a2f55
игорь кудрявцев <igorayn_112@mail.ru>
2019-04-14 17:30:34 +0300

    change category

--- backend/views/admin/tree-manager.php
+++ backend/views/admin/tree-manager.php
@@ -5,12 +5,14 @@ $this->title = 'Манагер меню';
 <div class="category">
 
 	<div class="category__main">
+
 		<div class="category__list" data-id="1">
 			<div class="main-block">
 				<span class="name-category">Категории</span>
 				<span class="add-category">&plus;</span>
 			</div>
 		</div>
+		
 	</div>
 
 </div>
--- backend/web/js/common.js
+++ backend/web/js/common.js
@@ -2,33 +2,53 @@ $(document).ready(function(){
 
 	// category
 	$(function() {
-
-		var addBtn = $('.add-category'),
-				list   = $('.category__list');
 		
-		addBtn.one('click', function(){
+		function addCategory() {
 
-			$(this).parent().parent().append('<form class="form-add">\
+			// добовляем форму 
+			$(this).parent().after('<form class="form-add">\
 					<input type="text" class="input-text" placeholder="Имя категории">\
 					<button class="submit-btn">Сохранить</button></form>');
-		});
 
-	});
+			// добавляем класс для анимации после динамического добавления элемента (form)
+			setTimeout(function() {
+				$('.form-add').addClass('visible');
+			},25);
 
-	$('body').on('click', 'button', function(evt){
-		evt.preventDefault();
-		var name = $(this).siblings('.input-text').val();
-		var id   = $(this).parents('.category__list').data('id');
-		var data = {
-			name: name,
-			id: id
+			// отменяем событие click на кнопке - добавления категории (+)
+			$('.category').off('click', '.add-category', addCategory);
+			
 		};
-		$.ajax({
-			type: 'POST',
-			data: data
-		});
-		$('.form-add').remove();
-	});
+		
+		// делегированное события клика на динамически добаленого элемента
+		$('.category').on('click', '.add-category', addCategory);
+		
+		function categoryAjax(evt) {
 
+			evt.preventDefault();//отменяем стандартное поведение кнопки отправки формы на сервер 
+
+			var name = $(this).siblings('.input-text').val();// считываем с инпута содержимое
+			var id   = $(this).parents('.category__list').data('id');// считываем атрибут id с родительского элемента
+
+			var data = {
+				name: name,// значение инпута
+				id: id// значение атрибута id
+			};
+
+			// отправляем ajax запрос на сервер
+			$.ajax({
+				type: 'POST',
+				data: data
+			});
+
+			$('.form-add').remove();// удаляем форму после отправки запроса на сервер
+			$('.category').on('click', '.add-category', addCategory);// возобновляем событие click на кнопку добавить категорию (+)
+
+		};
+
+		// при клике на кнопку вызываем функцию отправки формы на сервер
+		$('.category').on('click', 'button', categoryAjax);
+	
+});
 
 });
