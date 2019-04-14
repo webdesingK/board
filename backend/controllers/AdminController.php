f5d87888e7af06491d08758d9a9556134c1aee8c
mikle <mikle.bronn@gmail.com>
2019-04-14 10:09:13 +0300

    tree manager

--- backend/controllers/AdminController.php
+++ backend/controllers/AdminController.php
@@ -2,11 +2,14 @@
 
 namespace backend\controllers;
 
-class AdminController extends \yii\web\Controller
-{
-    public function actionIndex()
-    {
+class AdminController extends \yii\web\Controller {
+
+    public function actionIndex() {
         return $this->render('index');
+    }
+
+    public function actionTreeManager() {
+        return $this->render('tree-manager');
     }
 
 }
