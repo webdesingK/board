8f9e831f2f04624b57f02d3f90866a1308e420cd
webdesingK <igorayn_112@mail.ru>
2019-04-21 17:33:48 +0300

    запуск тестирования добавления категорий

--- backend/views/admin/tree-manager.php
+++ backend/views/admin/tree-manager.php
@@ -9,17 +9,18 @@ $this->title = 'Манагер меню';
 		<div class="category__list" data-id="1">
 			<div class="category__list-block">
 				<span class="name-category">Категории</span>
-				<span class="add-category">&plus;</span>
+				<span class="add-category" title="Добавить новую категорию">&plus;</span>
+				<span class="tabs-category" title="Развернуть">▼</span>
 			</div>
 		</div>
 		
 	</div>
-
+<!-- 
 	<div class="arr_obj">
 
 		<div class="block-js"></div>
 
-	</div>
+	</div> -->
 
 </div>
 <!-- /.category -->
--- backend/web/js/common.js
+++ backend/web/js/common.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 	$(function () {
 
 		function addCategory() {
-			console.log('dsf')
+
 			if ($(this).text() == '+') {
 				
 				$(this).html('&minus;');
@@ -24,7 +24,7 @@ $(document).ready(function () {
 			setTimeout(function () {
 				$('.form-add').addClass('visible');
 			}, 25);
-			// $('.category').on('click', '.not', addCategory);
+
 		};
 
 		// делегированное события клика на динамически добаленого элемента
@@ -38,20 +38,23 @@ $(document).ready(function () {
 			var id = $(this).parents('.category__list').data('id'); // считываем атрибут id с родительского элемента
 
 			var data = {
+				nameOfAction: 'create', 
 				name: name, // значение инпута
-				id: id // значение атрибута id
+				parentId: id // значение атрибута id
 			};
 
 			// отправляем ajax запрос на сервер
 			$.ajax({
 				type: 'POST',
-				data: data
+				data: data,
+				success: function(resp) {
+					$('.category__main').html(resp);
+				}
 			});
 
 			$(this).parent().siblings('.add-category').html('&plus;');
 			$(this).parent().siblings('.category__list-block').children('.add-category').html('&plus;');
 			$(this).parent().remove(); // удаляем форму после отправки запроса на сервер
-			// $('.category').on('click', '.add-category', addCategory);
 		};
 
 		// при клике на кнопку вызываем функцию отправки формы на сервер
@@ -62,122 +65,150 @@ $(document).ready(function () {
 
 	$(function() {
 
-		var block = $('.block-js');
+	// 	var block = $('.block-js');
 
-		var data = [
-			{
-				id: '1.1',
-				name: 'first',
-				enclus: [
-					{
-						id: '1.1.1',
-						name: 'first-one'
-					},
-					{
-						id: '1.1.2',
-						name: 'first-twoo',
-						enclus: [
-							{
-								id: '1.2.1',
-								name: 'first-twoo-one'
-							}
-						]
-					}
-				]
-			},
-			{
-				id: '1.2',
-				name: 'twoo',
-				enclus: [
-					{
-						id: '1.2.1',
-						name: 'twoo-one'
-					},
-					{
-						id: '1.2.2',
-						name: 'twoo-twoo',
-						enclus: [
-							{
-								id: '1.2.2.1',
-								name: 'twoo-twoo-one',
-								enclus: [
-									{
-										id: '1.2.2.2',
-										name: 'twoo-twoo-one-one'
-									}
-								]
-							}
-						]
-					}
-				]
-			},
-			{
-				id: '1.2',
-				name: 'twoo',
-				enclus: [
-					{
-						id: '1.2.1',
-						name: 'twoo-one'
-					},
-					{
-						id: '1.2.2',
-						name: 'twoo-twoo',
-						enclus: [
-							{
-								id: '1.2.2.1',
-								name: 'twoo-twoo-one',
-								enclus: [
-									{
-										id: '1.2.2.2',
-										name: 'twoo-twoo-one-one'
-									}
-								]
-							}
-						]
-					}
-				]
-			}
-		];
+	// 	var data = [
+	// 		{
+	// 			id: '1.1',
+	// 			name: 'first',
+	// 			enclus: [
+	// 				{
+	// 					id: '1.1.1',
+	// 					name: 'first-one'
+	// 				},
+	// 				{
+	// 					id: '1.1.2',
+	// 					name: 'first-twoo',
+	// 					enclus: [
+	// 						{
+	// 							id: '1.1.3',
+	// 							name: 'first-twoo-one'
+	// 						}
+	// 					]
+	// 				}
+	// 			]
+	// 		},
+	// 		{
+	// 			id: '1.2',
+	// 			name: 'twoo',
+	// 			enclus: [
+	// 				{
+	// 					id: '1.2.1',
+	// 					name: 'twoo-one'
+	// 				},
+	// 				{
+	// 					id: '1.2.2',
+	// 					name: 'twoo-twoo',
+	// 					enclus: [
+	// 						{
+	// 							id: '1.2.2.1',
+	// 							name: 'twoo-twoo-one',
+	// 							enclus: [
+	// 								{
+	// 									id: '1.2.2.2',
+	// 									name: 'twoo-twoo-one-one'
+	// 								}
+	// 							]
+	// 						}
+	// 					]
+	// 				}
+	// 			]
+	// 		},
+	// 		{
+	// 			id: '1.3',
+	// 			name: 'twoo',
+	// 			enclus: [
+	// 				{
+	// 					id: '1.3.1',
+	// 					name: 'twoo-one'
+	// 				},
+	// 				{
+	// 					id: '1.3.2',
+	// 					name: 'twoo-twoo',
+	// 					enclus: [
+	// 						{
+	// 							id: '1.3.2.1',
+	// 							name: 'twoo-twoo-one',
+	// 							enclus: [
+	// 								{
+	// 									id: '1.3.2.2',
+	// 									name: 'twoo-twoo-one-one'
+	// 								}
+	// 							]
+	// 						}
+	// 					]
+	// 				}
+	// 			]
+	// 		}
+	// 	];
 
-		function test (obj) {
-			var dataHtml = '';		
-			for(key in obj) {
-				dataHtml += `
-				<div class="category__list" data-id="${obj[key].id}">
-					<div class="category__list-block">
-						<span class="name-category">${obj[key].id}</span>
-						<span class="add-category">&plus;</span>
-					</div>
-					${obj[key].enclus !== undefined ? test(obj[key].enclus) : ''}
-				</div>
-				`;
-			}
-			return dataHtml;
-		};
+	// 	function test (obj) {
+	// 		var dataHtml = '';		
+	// 		for(key in obj) {
+	// 			dataHtml += `
+	// 			<div class="category__list" data-id="${obj[key].id}">
+	// 				<div class="category__list-block">
+	// 					<span class="name-category">${obj[key].id}</span>
+	// 					<span class="add-category" title="Добавить новую категорию">&plus;</span>
+	// 					<span class="tabs-category" title="Развернуть">▼</span>
+	// 				</div>
+	// 				${obj[key].enclus !== undefined ? test(obj[key].enclus) : ''}
+	// 			</div>
+	// 			`;
+	// 		}
+	// 		return dataHtml;
+	// 	};
 
-		block.html(
-			`<div class="category__list" data-id="1">
-					<div class="category__list-block">
-						<span class="name-category">1</span>
-						<span class="add-category">&plus;</span>
-					</div>
-					${test(data)}
-				</div>
-				`);
+	// 	block.html(
+	// 		`<div class="category__list" data-id="1">
+	// 				<div class="category__list-block">
+	// 					<span class="name-category">1</span>
+	// 					<span class="add-category" title="Добавить новую категорию">&plus;</span>
+	// 					<span class="tabs-category" title="Развернуть">▼</span>
+	// 				</div>
+	// 				${test(data)}
+	// 			</div>
+	// 			`);
 
-		var catList = $('.category__list');
+		// nested
+		var nestedList = $('.category__list');
 
 		(function delLastBorder(arr) {
 
 			for (var i = 0; i < arr.length; i++) {
+
 				if(arr.eq(i).children('.category__list').length == 0) {
 					arr.eq(i).addClass('after_none');
+					arr.eq(i).find('.tabs-category').remove();
 				}
+
 				arr.eq(i).children('.category__list:last').addClass('after_hide');
+
 			}
 
-		})(catList);
+		})(nestedList);
 
+		$('.category__list:not(:first)').addClass('none');
+		// tabs
+		var tabs = $('.tabs-category');
+
+		tabs.on('click', function() {
+
+			for (var i = 0; i < tabs.length; i++) {
+
+				if ($(this).parent().siblings().eq(i).find('.tabs-category').html() == '▲') {
+					$(this).parent().siblings().eq(i).find('.tabs-category').click();
+				}
+
+			}
+			if ($(this).html() == '▼') {
+				$(this).html('▲').attr('title', 'Свернуть');
+			} else{
+				$(this).html('▼').attr('title', 'Развернуть');
+			}
+			$(this).parent().siblings().toggleClass('none');
+
+		});
 	});
 
 });
