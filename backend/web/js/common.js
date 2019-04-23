e137143c49862baf3b32ec2bceef0aa189fa000c
webdesingK <igorayn_112@mail.ru>
2019-04-23 21:26:44 +0300

    сортировка отрытых категорий

--- backend/web/js/common.js
+++ backend/web/js/common.js
@@ -32,6 +32,37 @@ $(document).ready(function () {
 		// делегированное события клика на динамически добаленого элемента
 		$('.category').on('click', '.add-category', addCategory);
 
+		// функция передающая в ajax запрос с масивом блоков у которых нет класса 'none'
+		function sort(arr, number) {
+
+			var ar = [];
+
+			for (var i = 0; i < arr.length; i++) {
+				if (!arr.eq(i).hasClass('none')) {
+					ar.push(arr.eq(i).data('id'));
+				}
+			}
+
+			if (ar.indexOf(number) != '-1') {
+				ar.splice(ar.indexOf(number),1);
+			}
+			return ar;
+		};
+
+		function ajx(obj, numm) {
+
+			$.ajax({
+				type: 'POST',
+				data: obj,
+				success: function(resp) {
+					$('.category__main').html(resp);
+					tab($('.category__list'));
+					delLastBorder($('.category__list'));
+				}
+			});
+
+		}
+
 		function categoryAjax(evt) {
 
 			evt.preventDefault(); //отменяем стандартное поведение кнопки отправки формы на сервер 
@@ -42,37 +73,28 @@ $(document).ready(function () {
 			var data = {
 				nameOfAction: 'create', 
 				name: name, // значение инпута
-				id: id // значение атрибута id
+				id: id, // значение атрибута id
+				arrId: sort($('.category__list')) // сортировка открытых элементов
 			};
 
 			// отправляем ajax запрос на сервер
-			$.ajax({
-				type: 'POST',
-				data: data,
-				success: function(resp) {
-					$('.category__main').html(resp);
-					delLastBorder($('.category__list'));
-				}
-			});
+			ajx(data);
 
 		};
 
+		// отправка ajax запроса на удаления категории и если есть в ней подкотегории
 		$('.category').on('click', '.del-category', function() {
 
 			var id = $(this).parents('.category__list').data('id');
 			var data = {
 				nameOfAction: 'delete',
-				id: id
+				id: id,
+				arrId: sort($('.category__list'), id) // сортировка открытых элементов
 			};
 
-			$.ajax({
-				type: 'POST',
-				data: data,
-				success: function(resp) {
-					$('.category__main').html(resp);
-					delLastBorder($('.category__list'));
-				}
-			});
+			var numm = $(this).parents('.category__list').data('id');
+
+			ajx(data, numm);
 
 		});
 
@@ -107,20 +129,16 @@ $(document).ready(function () {
 
 			for (var i = 0; i < tabs.length; i++) {
 
-				if ($(this).parents('.category').siblings().eq(i).find('.add-category').html() == '-') {
-					console.log(i)
-				}
-
-				if ($(this).parent().siblings().eq(i).find('.tabs-category').html() == '▲') {
+				if ($(this).parent().siblings().eq(i).find('.tabs-category').html() == '▼') {
 					$(this).parent().siblings().eq(i).find('.tabs-category').click();
 				}
 
 			}
-			if ($(this).html() == '▼') {
-				$(this).html('▲').attr('title', 'Свернуть');
+			if ($(this).html() == '▶') {
+				$(this).html('▼').attr('title', 'Свернуть');
 				$(this).parents('.category__list').addClass('add-before');
 			} else{
-				$(this).html('▼').attr('title', 'Развернуть');
+				$(this).html('▶').attr('title', 'Развернуть');
 				$(this).parent().parent().removeClass('add-before');
 			}
 			$(this).parent().siblings().toggleClass('none');
@@ -130,8 +148,10 @@ $(document).ready(function () {
 		function tab(arr) {
 
 			for (var i = 0; i < arr.length; i++) {
-				if(arr.eq(i).html() == '▼'){
-					arr.eq(i).parents('.category__list').addClass('add-before');
+				if(!arr.eq(i).hasClass('none')){
+					arr.eq(i).addClass('add-before')
+					.find('.tabs-category').html('▼')
+					.attr('title', 'Свернуть');
 				}
 			}
 
