f03ef3a162248c157c11d191cdba1e04ef8763b0
webdesingK <igorayn_112@mail.ru>
2019-05-12 14:18:31 +0300

    добавление форм регистрации и входа

--- frontend/web/js/common.js
+++ frontend/web/js/common.js
@@ -3,45 +3,44 @@ $(document).ready(function() {
 	// menu
 	$(function() {
 
-		var menu       = $('.menu'),
+		let menu       = $('.menu'),
 				menuBtn    = $('#menu-btn'),
 				flagBtn    = true,
 				heightMenu = $('.menu__first').outerHeight(true);
 
+		// задаем всем menu__second высоту родителя, что-бы при сворачивании и разворачивании menu__second не перепрыгивали на новую строку 
 		$('.menu__second').each(function() {
 			$(this).height(heightMenu);
 		});
 
-		function clickMenuBtn() {
-			$(this).next().slideToggle('slow', function() {
-				hideSubMenu($('.menu__second'));
-				$(this).find('.menu__second:first').show();
-			}).removeClass('none');
-		};
-
-		menuBtn.on('click', clickMenuBtn);
-		menu.on('click', '#menu-close', function(){
-			menuBtn.click();
-		});
-
-		function hideSubMenu(obj) {
-			obj.each(function() {
-				$(this).hide();
+		// функция для скрытия всех категория 2 уровня
+		function hideSubMenu(obj) {// передаем массив обьектов которые нужно скрыть
+			obj.each(function() {// проходим циклом по нему
+				$(this).hide();// и скрываем их
 			});
 		};
 
-		hideSubMenu($('.menu__first li:not(:first) .menu__second'));
+		// при загрузке страницы запускаем функцию скрытия категория 2 уровня
+		hideSubMenu($('.menu__first li:not(:first) .menu__second'));// передаем в аргумент все категории 2 уровня кроме первого
 
-		function mouseShow() {
-			hideSubMenu($('.menu__second'));
-			$(this).children('.menu__second').show();
+		// функция для плавного открытия и закрытия меню
+		function clickMenuBtn() {
+			$(this).next().slideToggle('slow', function() {// callback функция 
+				hideSubMenu($('.menu__second'));// скрываем все категории второго уровня
+				$(this).find('.menu__second:first').show();// открываем самую первую категорию второго уровня 
+			}).removeClass('none');// удаляем класс none который задан для скрытия меню
 		};
 
-		menu.on('mouseenter', '.menu__first > li', mouseShow);
+		menuBtn.on('click', clickMenuBtn);// клик на кнопку меню вызывает функцию скрытия(открытия) меню
+
+    // клик на кнопку крестика(#menu-close) вызываем вункцию в которой
+		menu.on('click', '#menu-close', function(){
+			menuBtn.click();// эмитируем клик на кнопку меню для закрытия меню
+		});
 
 		// когда открыто меню при нажатии в любом месте кроме самого меню скрываем меню
 		$(document).mouseup(function (e){ // событие клика по веб-документу
-			var div = $('.menu'); // тут указываем ID элемента
+			let div = $('.menu'); // тут указываем ID элемента
 			if (!div.is(e.target) // если клик был не по нашему блоку
 			    && div.has(e.target).length === 0 // и не по его дочерним элементам
 			    && $('.menu__first').is(':visible')) { // и когда меню открыто
@@ -52,9 +51,76 @@ $(document).ready(function() {
 		// когда открыто меню при нажатии на esc скрываем меню
 		$(document).keyup(function(evt){
 			if (evt.keyCode == 27 && $('.menu__first').is(':visible')) menuBtn.click();
+			if (evt.keyCode == 27 && $('#sign-in').is(':visible')) $('#sign-in').addClass('none');
+			if (evt.keyCode == 27 && $('#sign-up').is(':visible')) $('#sign-up').addClass('none');
+		});
+
+		// функция при наведении на категорию 1 уровня (эфект hover)
+		function mouseShow(self) {
+			hideSubMenu($('.menu__second'));// скрываем открытые категории 2 уровня
+			self.siblings('.menu__second').show();// и открываем категорию 2 уровня на которой наведен курсор
+		};
+
+		let startX,
+				x,
+				flagMenuActive = true,
+				set = setInterval(fn,4);
+				clearInterval(set);
+
+		menu.on('mousemove', '.menu__first > li > a', function(evt) {
+			 x = Math.round(evt.pageX - $(this).offset().left);
+		});
+
+		function fn(self) {
+			if (startX > x && flagMenuActive) {
+				mouseShow(self);
+				clearInterval(set);
+			}
+			startX++;
+		};
+
+		menu.on('mouseenter', '.menu__second', function(){
+			flagMenuActive = false;
+		});
+
+		menu.on('mouseenter', '.menu__first > li > a', function(evt) {
+
+			startX = Math.round(evt.pageX - $(this).offset().left);
+			flagMenuActive = true;
+			
+			clearInterval(set);
+			set = setInterval(fn, 15, $(this));
+
 		});
 
 	});
 
+	// auth
+	$(function() {
+
+		let auth = $('.auth');
+
+		auth.on('click', '#auth', function(){
+			$('#sign-in').toggleClass('none');
+		});
+
+		auth.on('click', '#sign-in span', function() {
+
+			$('#sign-in').addClass('none');
+			$('#sign-up').removeClass('none');
+
+		});
+		auth.on('click', '#sign-up span', function() {
+
+			$('#sign-up').addClass('none');
+			$('#sign-in').removeClass('none');
+
+		});
+
+		auth.on('click', '#auth-user', function(){
+			$('.auth__user').slideToggle().removeClass('none');
+		});
+
+	});
 
 });
