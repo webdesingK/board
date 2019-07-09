a31a44a76bc75c2a6c98343c8ce7b05b596b2325
webdesingK <igorayn_112@mail.ru>
2019-07-09 21:50:21 +0300

    backend - создание фильтров

--- backend/views/admin/create-filters.php
+++ backend/views/admin/create-filters.php
@@ -3,9 +3,24 @@
 $this->title = 'Создание фильтров';
 
 //$this->registerCssFile('@web/css/main.css');
-$this->registerJsFile('@web/js/common.js');
+$this->registerJsFile('@web/js/create-filters.js');
+$this->registerCssFile('@web/css/create-filters.css');
 
 ?>
 
-Создание фильтров
-
+<div class="wrapeer">
+	<div class="wrap">
+		<div class="wrap__left">
+			<div class="name-error none">Введите название!!!</div>
+			<input class="filter-name" type="text" placeholder="Название фильтра">
+		</div>
+		<div class="wrap__right">
+			<div class="wrap__right-add">
+				<div class="filter__add" title="добавить в список фильтра">✚</div>				
+			</div>
+			<div class="btn-wrap">
+				<div class="btn-save">Сохранить</div>
+			</div>
+		</div>
+	</div>
+</div>
