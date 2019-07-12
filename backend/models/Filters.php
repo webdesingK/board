2d560850f8ecd63773b10815eb3f327c4f852630
miklebronn <mikle.bronn@gmail.com>
2019-07-12 18:30:50 +0300

    Работа над action привязка фильтров (возврат произвольного массива при ajax запросе)

--- backend/models/Filters.php
+++ backend/models/Filters.php
@@ -9,34 +9,43 @@
 
         static public function createFilter(array $data) {
 
-            $db = new Query();
-
             $tableName = $data['name'];
-
             $titles = [];
 
             foreach ($data['arrList'] as $datum) {
                 $titles[] = [$datum];
             }
 
-            if ($db->select('table_name')->from('information_schema.tables')->where('table_name = :tableName')->addParams([':tableName' => $tableName])->all()) {
+            $db = new Query();
+
+            try {
+
+                if ($db->select('table_name')->from('information_schema.tables')->where('table_name = :tableName')->addParams([':tableName' => $tableName])->all()) {
+                    return [
+                        'status' => false,
+                        'text' => 'Такой фильтр уже существует'
+                    ];
+                }
+
+                $db->createCommand()->createTable($tableName, [
+                    'id' => 'pk',
+                    'title' => 'string'
+                ], 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB')->execute();
+
+                $db->createCommand()->batchInsert($tableName, ['title'], $titles)->execute();
+
+                return [
+                    'status' => true,
+                    'text' => 'Фильтр ' . $tableName . ' успешно создан'
+                ];
+
+            }
+            catch (\Throwable $e) {
                 return [
                     'status' => false,
-                    'text' => 'Такой фильтр уже существует'
+                    'text' => 'Ошибка базы данных'
                 ];
             }
-
-            $db->createCommand()->createTable($tableName, [
-                'id' => 'pk',
-                'title' => 'string'
-            ], 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB')->execute();
-
-            $db->createCommand()->batchInsert($tableName, ['title'], $titles)->execute();
-
-            return [
-                'status' => true,
-                'text' => 'Фильтр ' . $tableName . ' успешно создан'
-            ];
 
         }
 
