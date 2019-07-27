1d9e472474349b1cc792909eb78a2751967ca4f3
miklebronn <mikle.bronn@gmail.com>
2019-07-27 15:43:16 +0300

    Закончил работу над фильтрами на админке

--- console/controllers/TestController.php
+++ console/controllers/TestController.php
@@ -4,18 +4,48 @@
 namespace console\controllers;
 
 use yii\console\Controller;
-use yii\helpers\Console;
+use yii\db\Migration;
 
 class TestController extends Controller {
 
-    public function actionTest() {
+    public function actionRun() {
 
-        Console::startProgress(0, 100);
-        for ($n = 1; $n <= 100; $n++) {
-            usleep(100000);
-            Console::updateProgress($n, 100);
+        try {
+
+            $db = \Yii::$app->db;
+
+            $migration = new Migration();
+
+            $db
+                ->createCommand('CREATE DATABASE filters CHARACTER SET utf8 COLLATE utf8_general_ci')
+                ->execute();
+
+            $db
+                ->createCommand()
+                ->createTable('filters.filters', [
+                    'id' => $migration->primaryKey()->unsigned(),
+                    'rusName' => $migration->string(50),
+                    'latName' => $migration->string(50),
+                    'url' => $migration->string(50),
+                    'idParentCategory' => $migration->smallInteger()
+                ], 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB')
+                ->execute();
+            $db
+                ->createCommand()
+                ->createTable('filters.filtersCategories', [
+                    'idFilter' => $migration->smallInteger()->unsigned(),
+                    'idCategory' => $migration->smallInteger()->unsigned(),
+                ], 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB')
+                ->execute();
+
+            echo 'Усё окей';
+            echo "\n";
+
         }
-        Console::endProgress();
+        catch (\Throwable $e) {
+            echo $e->getMessage();
+            echo "\n";
+        }
 
     }
 
